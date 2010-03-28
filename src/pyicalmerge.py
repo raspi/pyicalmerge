898b2c1dd46ba399bd6bb23c299a92c8da8e53b5
pekka.jarvinen <pekka.jarvinen@df8f3f1e-16e2-be95-21c9-830f4dc576e8>
2010-03-28 18:31:03 +0000

    First version added

---
+++ src/pyicalmerge.py
@@ -0,0 +1,75 @@
+#!/bin/env/python
+# -*- coding: utf8 -*-
+# Combine iCalendar files
+# $Id$
+# uses http://pypi.python.org/pypi/icalendar/2.1
+
+import os
+import sys
+import traceback
+import datetime
+import glob
+import codecs
+
+from ConfigParser import ConfigParser
+from optparse import OptionParser, Option, OptionGroup
+
+# 3rd party libs
+import vobject
+
+
+__AUTHOR__ = u"Pekka Järvinen"
+__YEAR__ = "2010"
+__VERSION__ = "0.0.1"
+
+
+if __name__ == "__main__":
+    banner  = u" %s" % (__VERSION__)
+    banner += u" (c) %s %s" % (__AUTHOR__, __YEAR__)
+
+    examples = []
+    examples.append("")
+
+    usage = "\n".join(examples)
+
+    parser = OptionParser(version="%prog " + __VERSION__, usage=usage, description=banner)
+
+    parser.add_option("--files", "-f", action="store", type="string", dest="files", help="Files match (Default: *.ics)", default="*.ics")
+    parser.add_option("--ical", "-i", action="store", type="string", dest="icalfile", help="iCalendar file output")
+
+    (options, args) = parser.parse_args()
+
+    if options.icalfile == "":
+        options.icalfile = None
+
+
+    if options.icalfile != None:
+        options.icalfile = os.path.realpath(options.icalfile)
+
+        files = glob.glob(options.files)
+
+        combinedCalendar = vobject.iCalendar()
+
+        for i in files:
+            print "Opening '%s'.." % i
+            f = open(i, 'rb')
+
+            print "Reading '%s'.." % i
+            contents = f.read()
+            contents = contents.decode('utf-8')
+
+            f.close()
+
+            for component in vobject.readComponents(contents):
+                combinedCalendar.add(component)
+
+        # Write iCal file
+        print "Writing iCalendar file '%s'.." % options.icalfile
+        f = open(options.icalfile, 'wb')
+        f.write(combinedCalendar.serialize())
+        f.close()
+
+        print "Done."
+        sys.exit(0)
+
+    sys.exit(1)
